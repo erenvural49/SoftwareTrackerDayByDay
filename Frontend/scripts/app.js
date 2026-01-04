80493e69463c2851c7f48cfa8b6c6bded7f6b690
erenvural49 <eren.vural@marun.edu.tr>
2026-01-04 13:39:07 +0300

    API URL UPDATED

--- Frontend/scripts/app.js
+++ Frontend/scripts/app.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:5277/api';
+const API_BASE_URL = "https://overinsistent-anachronically-justa.ngrok-free.dev/";
 
 // State Management
 let currentUser = null;
